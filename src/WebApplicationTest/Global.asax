e88b5e42618e12d24b9ceb9e592aa40c8b1050d2
ChrisHwang <chrishwang@live.cn>
2017-10-18 02:41:08 +0800

    upgrade to .net core 2.0, add VarintBitConverter

--- src/WebApplicationTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTest.Global" Language="C#" %>
---
+++ src/src/ConfigTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Aolyn.Config;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var builder = new ConfigurationBuilder()
+			//	.AddJsonFile("appsettings.json", true, true);
+			//var configuration = builder.Build();
+
+			//ConfigurationManager.Initialize(configuration);
+
+			//var optionsBuilder = new DbContextOptionsBuilder();
+
+			//ConfigurationManager.Entity.Use(optionsBuilder, "n");
+			//ConfigurationManager.Entity.Use(optionsBuilder, "ss");
+			//ConfigurationManager.Entity.Use(optionsBuilder, "sl");
+
+			var csn = ConfigurationManager.ConnectionStrings["n"];
+			var ss = ConfigurationManager.ConnectionStrings["ss"];
+			var sl = ConfigurationManager.ConnectionStrings["sl"];
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
